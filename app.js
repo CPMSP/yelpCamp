5e9ae70965116db85f9be6ed68bbfa598aaa2980
CPMSP <cpetersonmsp@gmail.com>
2020-03-02 12:06:38 -0600

    new heroku DB connection

--- app.js
+++ app.js
@@ -17,7 +17,21 @@ const   commentRoutes = require('./routes/comments'),
         campgroundRoutes = require('./routes/campgrounds'),
         indexRoutes = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/yelpCamp', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = process.env.MONGODB_URL;
+
+const connectDB = async () => {
+    try {
+    await mongoose.connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    });
+    console.log("MongoDB is Connected...");
+    } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+    }
+};
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
